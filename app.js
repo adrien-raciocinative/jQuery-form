0648130fb8fea815069a9bd0e94f491364477d06
adrien-raciocinative <adrien.ratiocinativesolution@gmail.com>
2022-07-12 17:13:33 +0530

    adding lenght validation on all the other fields and validation on checking password compatibility with confirm password

--- app.js
+++ app.js
@@ -4,51 +4,127 @@ $(document).ready(function () {
   var form = $("#myform");
   var name = $("#name");
   var email = $("#email");
+  var pass = $("#pass");
+  var cpass = $("#cpass");
 
-  function validateOnKeypress(field) {
-    $("input").keyup(function () {
-      if ($(field).val().length <= 2) {
-        $(field).css("border", "1px solid red");
-        $(field).next().css("display", "inline-block");
-        $(field).next().html("field should be atleat 2 characters");
-        $(field).next().css("color", "red");
+  // remove space in forms
+  // function noSpace(field) {
+  //   $(field).change(function () {
+  //     $(this).val($(this).val().trim());
+  //   });
+  //   console.log(this);
+  // }
+  // noSpace(name);
+  // noSpace(pass);
+  // noSpace(email);
+  // noSpace(cpass);
+
+  // // on keyup remove space
+  // function trimSpace(x) {
+  //   let field = $(x).attr("id");
+  //   $(x).blur(function () {
+  //     if (!/\S/.test($(x).val())) {
+  //       alert("form can not be only space");
+  //     } else {
+  //     }
+  //   });
+  // }
+
+  // trimSpace(name);
+
+  // validates form on keyup
+
+  // validate if password is equal to confirm password
+
+  function validatePassAndCpass() {
+    let pwdmt = $(".pswdmatch");
+    if (pass.val() != cpass.val()) {
+      pwdmt.html("Password does not match");
+      pwdmt.css("color", "red");
+      $(pass).css("border", "1px solid red");
+      $(cpass).css("border", "1px solid red");
+      $flag = false;
+    } else {
+      pwdmt.html("Password match");
+      pwdmt.css("color", "green");
+      $(pass).css("border", "1px solid green");
+      $(cpass).css("border", "1px solid green");
+    }
+  }
+
+  function valall(x) {
+    let field = $(x).attr("id");
+    $(x).keyup(function () {
+      if ($(x).val().length <= 2) {
+        $(x).css("border", "1px solid red");
+        $(x).next().css("display", "inline-block");
+        $(x)
+          .next()
+          .html(field + "" + " should be atleat 2 characters");
+        $(x).next().css("color", "red");
       } else {
-        $(field).css("border", "1px solid green");
-        $(field).next().css("color", "green");
-        $(field).next().html("nice job dude");
+        $(x).css("border", "1px solid green");
+        $(x).next().css("color", "green");
+        $(x).next().html("nice job dude");
       }
     });
   }
-  validateOnKeypress(name);
+  valall(name);
+  valall(pass);
+  valall(email);
+  valall(cpass);
 
-  function validateOnKeypress(field) {
-    $("input").keyup(function () {
-      if ($(field).val().length <= 2) {
-        $(field).css("border", "1px solid red");
-        $(field).next().css("display", "inline-block");
-        $(field).next().html("field should be atleat 2 characters");
-        $(field).next().css("color", "red");
-      } else {
-        $(field).css("border", "1px solid green");
-        $(field).next().css("color", "green");
-        $(field).next().html("nice job dude");
-      }
-    });
+  // validatepassword
+  function validatesPass() {
+    if ($(pass).val().length < 2) {
+      $(pass).css("border", "1px solid red");
+      $(pass).next().css("display", "inline-block");
+      $flag = false;
+    } else {
+      $(pass).css("border", "1px solid green");
+      $(pass).next().css("display", "none");
+      $flag = true;
+    }
   }
-  validateOnKeypress(email);
+
+  // validatepassword
+  function validatescPass() {
+    if ($(cpass).val().length < 2) {
+      $(cpass).css("border", "1px solid red");
+      $(cpass).next().css("display", "inline-block");
+      $flag = false;
+    } else {
+      $(cpass).css("border", "1px solid green");
+      $(cpass).next().css("display", "none");
+      $flag = true;
+    }
+  }
+  function validatesPass() {
+    if ($(pass).val().length < 2) {
+      $(pass).css("border", "1px solid red");
+      $(pass).next().css("display", "inline-block");
+      $flag = false;
+    } else {
+      $(pass).css("border", "1px solid green");
+      $(pass).next().css("display", "none");
+      $flag = true;
+    }
+  }
 
   // validateemail
   function validatesEmail() {
-    if ($(name).val().length < 2) {
+    if ($(email).val().length < 2) {
       $(email).css("border", "1px solid red");
       $(email).next().css("display", "inline-block");
       $flag = false;
     } else {
       $(email).css("border", "1px solid green");
       $(email).next().css("display", "none");
+      $flag = true;
     }
   }
 
+  // validatename
   function validateName() {
     if ($(name).val().length < 2) {
       $(name).css("border", "1px solid red");
@@ -57,19 +133,23 @@ $(document).ready(function () {
     } else {
       $(name).css("border", "1px solid green");
       $(name).next().css("display", "none");
+      $flag = true;
     }
   }
 
-  $(form).submit(function () {
+  $(form).submit(function (e) {
+    let modal = $(".modals");
+    e.preventDefault();
     validateName();
     validatesEmail();
-    console.log(
-      $(form)
-        .find(inputs)
-        .each(function () {
-          $(this).val().trim();
-        })
-    );
+    validatesPass();
+    validatescPass();
+    validatePassAndCpass();
+    console.log($flag);
+
+    if ($flag === true) {
+      modal.css("display", "block");
+    }
     return $flag;
   });
 });
