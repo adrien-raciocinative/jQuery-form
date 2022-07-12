6b96d6b924b0c3ceed085ea38d3f0317356508d7
adrien-raciocinative <adrien.ratiocinativesolution@gmail.com>
2022-07-12 14:00:55 +0530

    adding validation to email

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   var inputs = $("input");
   var form = $("#myform");
   var name = $("#name");
+  var email = $("#email");
 
   function validateOnKeypress(field) {
     $("input").keyup(function () {
@@ -20,6 +21,34 @@ $(document).ready(function () {
   }
   validateOnKeypress(name);
 
+  function validateOnKeypress(field) {
+    $("input").keyup(function () {
+      if ($(field).val().length <= 2) {
+        $(field).css("border", "1px solid red");
+        $(field).next().css("display", "inline-block");
+        $(field).next().html("field should be atleat 2 characters");
+        $(field).next().css("color", "red");
+      } else {
+        $(field).css("border", "1px solid green");
+        $(field).next().css("color", "green");
+        $(field).next().html("nice job dude");
+      }
+    });
+  }
+  validateOnKeypress(email);
+
+  // validateemail
+  function validatesEmail() {
+    if ($(name).val().length < 2) {
+      $(email).css("border", "1px solid red");
+      $(email).next().css("display", "inline-block");
+      $flag = false;
+    } else {
+      $(email).css("border", "1px solid green");
+      $(email).next().css("display", "none");
+    }
+  }
+
   function validateName() {
     if ($(name).val().length < 2) {
       $(name).css("border", "1px solid red");
@@ -31,9 +60,9 @@ $(document).ready(function () {
     }
   }
 
-  $(form).submit(function (e) {
-    e.preventDefault();
+  $(form).submit(function () {
     validateName();
+    validatesEmail();
     console.log(
       $(form)
         .find(inputs)
